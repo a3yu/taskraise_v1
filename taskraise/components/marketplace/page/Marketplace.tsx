c7d9f0bcbfe27c9373ca5fbb916e8a0834bcf060
a3yu <74028906+a3yu@users.noreply.github.com>
2024-02-01 22:44:08 -0500

    Update Marketplace.tsx

--- taskraise/components/marketplace/page/Marketplace.tsx
+++ taskraise/components/marketplace/page/Marketplace.tsx
@@ -215,15 +215,12 @@ function Marketplace({
               key={ticket.id}
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.4,
-                ease: [0.25, 0.25, 0, 1],
-              }}
             >
               <Service service={ticket} />
             </motion.div>
           );
         })}
+        {isLoading && <h2>Loading...</h2>}
       </div>
     </div>
   );
